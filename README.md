48876511f8df76b38e1923d92e3ee64394533809
sweedalcristimathias <sweedalcristimathias@gmail.com>
2024-01-14 21:35:11 +0530

    finished

--- README.md
+++
@@ -1,5 +0,0 @@
-# angular-14-template-driven-form-validation-example
-
-Angular 14 - Template-Driven Form Validation Example
-
-Documentation at https://jasonwatmore.com/post/2022/12/19/angular-14-template-driven-form-validation-example"# templateangular" 
---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+﻿import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({ selector: 'app-root', templateUrl: 'app.component.html' })
+export class AppComponent {
+    onSubmit(f: NgForm) {
+        // stop here if form is invalid
+        if (f.invalid) {
+            return;
+        }
+        alert("Registered successfully")
+
+        // alert('SUCCESS!! :-)\n\n' + JSON.stringify(f.value, null, 4));
+    }
+}
